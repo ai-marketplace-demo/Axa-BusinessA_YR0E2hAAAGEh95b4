6a56cad3dbc22293c6e4f786d2fc0b791bbc7d1f
Ait el harraj <haramine@amazon.fr>
2021-01-22 11:33:21 +0100

    Fix commit.js

--- src/config.js
+++ src/config.js
@@ -8,19 +8,12 @@ const conf = {
     apiGateway: {
         REGION: "eu-west-1",
 
-        URL: " https://lmc1x2k4ue.execute-api.eu-west-1.amazonaws.com/prod/graphql/api", // local dev https
-        ESURL: " https://lmc1x2k4ue.execute-api.eu-west-1.amazonaws.com/prod/graphql/api", // local dev https
+        URL: " https://lmc1x2k4ue.execute-api.eu-west-1.amazonaws.com/prod/graphql/api",
+        ESURL: " https://lmc1x2k4ue.execute-api.eu-west-1.amazonaws.com/prod/graphql/api",
 
-        //URL: "https://iqqb86uk6l.execute-api.eu-west-1.amazonaws.com/prod/graphql/api", // 950130011294
-        //ESURL: "https://iqqb86uk6l.execute-api.eu-west-1.amazonaws.com/prod/search/api", // 950130011294
     },
     cognito: {
         REGION: "eu-west-1",
-
-        //USER_POOL_ID: "eu-west-1_ovuLWv48G",//950130011294
-        //APP_CLIENT_ID: "1s0bibvbbdcb1mb3qbq9afu9ti",//950130011294
-        //DOMAIN:"https://datahubdeveuwest1950130011294.auth.eu-west-1.amazoncognito.com", //950130011294
-        //REDIRECT_URI : 'https://d26bwgcp8nv25z.cloudfront.net', // my deployment
 
         USER_POOL_ID: "eu-west-1_u2i6wjYQH",
         APP_CLIENT_ID: "7hibugnidkcp8d85e9b3qf3pa7",
