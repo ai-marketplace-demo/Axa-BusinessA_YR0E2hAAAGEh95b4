afa361a4330bd801e047566525c489ace5b8c6e0
mamallem <mamallem@amazon.com>
2021-03-23 07:44:31 +0100

    Fix config

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const config = {
         USER_POOL_ID: "eu-west-1_LHoa6avN8",
         APP_CLIENT_ID: "6mub43r79g5iu3hm4gelj5i7e3",
         DOMAIN: "datahubgammaeuwest1532999248425.auth.eu-west-1.amazoncognito.com",
-        REDIRECT_URI: "https://localhost:8080",
+        REDIRECT_URI: "https://datahub.haramine.people.aws.dev",
         SIGNOUT_URL: "https://datahub.haramine.people.aws.dev",
         TYPE:"token",
         SCOPE:['email','phone','openid','profile']
